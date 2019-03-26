aeccb74338bc40c16d80ec2a54b12a07bb289762
kduong@stuy.edu <kduong@stuy.edu>
2019-03-26 19:30:59 -0400

    Finished and tested more

--- UserOfList.java
+++ UserOfList.java
@@ -29,27 +29,39 @@ public class UserOfList {
 
 
         System.out.println("Testing Add");
-        list.add(3, "d");
-        System.out.println("Adding \"d\" at element 3: " + list);
+        list.add(3, "g");
+        System.out.println("Adding \"g\" at element 3: " + list);
         list.add(0, "K");
         System.out.println("Adding \"K\" at the head: " + list);
-        list.add(1, "l");
+        list.add(5, "l");
         System.out.println("Adding \"l\" at element 1: " + list);
-        list.add(list.size(), "j");
-        System.out.println("Adding \"j\" at the end: " + list);
+        list.add(list.size(), "g");
+        System.out.println("Adding \"g\" at the end: " + list);
+        list.add(6, "e");
+        System.out.println("Adding \"e\" at element 6: " + list);
 
         System.out.println("Testing Get");
         for (int i = 0; i < list.size(); i ++) {
           System.out.println("Element " + i + " : " + list.get(i));
         }
 
-        /*System.out.println("\nTesting Set");
-        list.set(3, "e");
-        System.out.println("Setting element 3 to \"e\": " + list);*/
+        System.out.println("\nTesting Set");
+        list.set(2, "e");
+        System.out.println("Setting element 2 to \"e\": " + list);
+        list.set(0, "P");
+        System.out.println("Setting element 1 to \"P\": " + list);
+        list.set(1, "w");
+        System.out.println("Setting element 3 to \"w\": " + list);
+        list.set(7, "g");
+        System.out.println("Setting element 7 to \"g\": " + list);
+        list.set(list.size() - 1, "S");
+        System.out.println("Setting last element to \"S\": " + list);
 
         System.out.println("\nTesting Remove");
         list.remove(3);
         System.out.println("Removing element 3: " + list);
+        list.remove(1);
+        System.out.println("Removing element 1: " + list);
 
         /*System.out.println("*****Test Using Recursive*****");
         List_inChainOfNodes_recursive list1 = new List_inChainOfNodes_recursive();
